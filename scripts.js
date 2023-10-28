b5800fb886bd247f2a93d1747380da29dfe272e4
LotteCodesThat <149241641+LotteCodesThat@users.noreply.github.com>
2023-10-28 15:32:23 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,3 +4,19 @@ document.body.addEventListener('mousemove', e =>
     const y = Math.floor(e.clientY / window.innerHeight * 255);
     document.body.style.background = `rgb(${x}, ${y}, 100)`;
 });
+
+const lettersDiv = document.getElementById('letters');
+const output = document.getElementById('output');
+const alphabet = 'RZKTFXEUGHMIJLPNBOCSVWYAQD';
+const incorrectAlphabet = 'XZABCEGTHIJLWNOPQRSDYUVMFK';
+
+for (let i = 0; i < alphabet.length; i++) {
+    const letterDiv = document.createElement('div');
+    letterDiv.textContent = alphabet[i];
+    letterDiv.className = 'letter';
+    lettersDiv.appendChild(letterDiv);
+
+    letterDiv.addEventListener('click', () => {
+        output.textContent += incorrectAlphabet[i];
+    });
+}

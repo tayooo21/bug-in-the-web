70a0059ca940355eeed5ef1f749257e210a8f013
Shayan456 <149245485+Shayan456@users.noreply.github.com>
2023-10-29 12:39:40 +0000

    Add files via upload

---
+++ Complete code/scripts.js
@@ -0,0 +1,73 @@
+document.body.addEventListener('mousemove', e => 
+{
+    // The background colour will change, depending on cursor placement on screen
+    const x = Math.floor(e.clientX / window.innerWidth * 255);
+    const y = Math.floor(e.clientY / window.innerHeight * 255);
+    document.body.style.background = `rgb(${x}, ${y}, 100)`;
+});
+
+const image1 = document.getElementById("image1");
+
+const lettersDiv = document.getElementById('letters');
+const output = document.getElementById('output');
+// Provide user with letters to select, being in a confusing order 
+const alphabet = 'RZKTFXEUGHMIJLPNBOCSVWYAQD';
+// Depending on their placement, the letters would translate to incorrect letters 
+const incorrectAlphabet = 'XZABCEGTHIJLWNOPQRSDYUVMFK';
+
+for (let i = 0; i < alphabet.length; i++) 
+{
+    const letterDiv = document.createElement('div');
+    letterDiv.textContent = alphabet[i];
+    letterDiv.className = 'letter';
+    lettersDiv.appendChild(letterDiv);
+
+    letterDiv.addEventListener('click', () => 
+    {
+        output.textContent += incorrectAlphabet[i];
+    });
+}
+
+const backspaceDiv = document.createElement('div');
+backspaceDiv.textContent = 'BACK';
+backspaceDiv.className = 'letter';
+lettersDiv.appendChild(backspaceDiv);
+
+backspaceDiv.addEventListener('click', () => 
+{
+    output.textContent = output.textContent.slice(0, -1);
+});
+
+let imageSelected = false;
+
+function checkAnswer(imageId) 
+{
+    if (imageId === 'image1') 
+    { 
+        imageSelected = true;
+    } 
+    else 
+    {
+        imageSelected = false;
+    }
+}
+
+enter.addEventListener('click', () => 
+{
+    if (output.textContent === 'BOTTLE' && imageSelected) 
+    {
+        //window.location.href = '../intermission/inter.html';
+        goToNextpage();
+    } 
+    else 
+    {
+        output.textContent = '';
+        imageSelected = false;
+    }
+});
+
+const nextPage = document.getElementById("next");
+
+function goToNextpage() {
+    nextPage.click();
+}
